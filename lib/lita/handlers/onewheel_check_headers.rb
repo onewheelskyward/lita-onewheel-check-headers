003fa895431ce4642e1cca6fbd4fb78714585e73
Andrew Kreps <andrew.kreps@gmail.com>
2016-10-23 15:02:05 -0700

    Changed the config around.

--- lib/lita/handlers/onewheel_check_headers.rb
+++ lib/lita/handlers/onewheel_check_headers.rb
@@ -3,21 +3,30 @@ require 'rest_client'
 module Lita
   module Handlers
     class OnewheelCheckHeaders < Handler
-      config :urls
-      config :headers
+      config :default
+      config :servers
 
-      route(/^lllrev$/i, :handle_lll_rev, command: true,
+      route(/^lllrev\s*(.*)$/i, :handle_lll_rev, command: true,
             help: { '!lllrev' => 'Get the config-specified headers from the config-specified urls.'})
 
       def handle_lll_rev(response)
         reply = ''
 
-        config.urls.each do |url|
+        server = response.matches[0][0]
+        if server.empty?
+          server = config.default
+        end
+
+        puts "#{server}"
+        puts config.servers
+        check_this = config.servers[server.to_sym]
+
+        check_this[:urls].each do |url|
           puts "Getting #{url}"
-          puts "Headers to check #{config.headers.inspect}"
+          puts "Headers to check #{check_this[:headers].inspect}"
           rest_client_get = RestClient.get url
           rest_client_get.headers.each do |header, value|
-            if config.headers.include? header.to_s
+            if check_this[:headers].include? header.to_s
               puts "Adding to reply: #{url} #{header}: #{value}"
               reply += "#{url} #{header}: #{value}\n"
             end
